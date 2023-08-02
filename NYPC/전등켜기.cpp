5e4e055c0cfa9c25f5b3ae60abddb78c2121cc36
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2023-08-02 13:37:09 +0900

    Success Code!
    happydesu

--- NYPC/전등켜기.cpp
+++ NYPC/전등켜기.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 using namespace std;
 char map[30][30] = { 0, };
-int dw[4] = { 0,0,-1,1 };
-int dh[4] = { -1,1,0,0 }; // 상 하 좌 우
+int dw[4] = { 0,1,0,-1 }; // 상, 하, 우, 좌
+int dh[4] = { -1,0,1,0 }; // -> 시계(+1) <- 반시계(+3)
 
 int main() {
 	int w, h; // 가로(열), 세로(행)
@@ -23,16 +23,19 @@ int main() {
 		}
 	}
 	for (int i = 0; i < 4; i++) {
-		int nx = x + dh[i];
-		int ny = y + dw[i];
 		int index = i;
+		int nx = x;
+		int ny = y;
 		while (true) {
+			nx = nx + dh[index];
+			ny = ny + dw[index];
+
 			if (nx < 0) {
 				direction.push_back('U');
 				location.push_back(ny);
 				break;
 			}
-			else if (nx >= w) {
+			else if (nx >= h) {
 				direction.push_back('D');
 				location.push_back(ny);
 				break;
@@ -42,35 +45,27 @@ int main() {
 				location.push_back(nx);
 				break;
 			}
-			else if (ny >= h) {
+			else if (ny >= w) {
 				direction.push_back('R');
 				location.push_back(nx);
 				break;
 			}
-			else { // 상하좌우
+			else {
 				if (map[nx][ny] == '\\') {
-					// nx = nx + dw[i];
-					// ny = ny + dh[i];
-					i = (i + 2) % 4;
-					nx = nx + dh[i];
-					ny = ny + dw[i];
-				}
-				else if (map[nx][ny] == '.') {
-					nx = nx + dh[i];
-					ny = ny + dw[i];
+					if ((index % 2) == 0) index = (index + 3) % 4; // 상 하 -> 반시계
+					else index = (index + 1) % 4; // 우 좌 -> 시계
 				}
 				else if (map[nx][ny] == '/') {
-					// nx = nx - dw[i];
-					// ny = ny - dh[i];
-					if (i == 0) i = (i + 3) % 4;
-					else if (i == 2) i = 1;
-					else i = (i + 1) % 4;
-					nx = nx + dh[i];
-					ny = ny + dw[i];
-				}
+					if ((index % 2) == 0) index = (index + 1) % 4; // 상 하 -> 시계
+					else index = (index + 3) % 4; // 우 좌 -> 반시계
+				} 
+				// '.'가 필요 없음(방향 바꾸지 않으니까)
 			}
 		}
-		i = index;
 	}
-	cout << direction.back() << " " << location.back() << endl;
+	//for (int i = 0; i < 4; i++) {
+	//	cout << direction[i] << " " << location[i] << '\n';
+	//}
+
+	cout << direction.front() << " " << location.front() << endl;
 }
