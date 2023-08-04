c08c23e324f49da97a999be7db5434a8a1e2897f
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2023-08-04 21:06:48 +0900

    incomplete

---
+++ NYPC/2016/SpinAndSlide.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+char map[100][100];
+
+void SpinAndSlide() {
+	char spin[100][100];
+
+}
+
+int main() {
+	int N, M; // N = 게임판의 크기
+	cin >> N >> M; // M = Spin and Slide 횟수 
+	for (int i = 0; i < N; i++) {
+		string str;
+		for (int j = 0; i < str.length(); j++) {
+			map[i][j] = str[j];
+		}
+	}
+
+}
